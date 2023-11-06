855805cb694d4f86530bdcbb8b00fbba2d8b673d
haowong <haowong.seu@gmail.com>
2023-11-06 11:26:12 +0800

    update

---
+++ SMGI_Common/TIN.cs
@@ -0,0 +1,133 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SMGI_Common
+{
+    public class TIN
+    {
+
+
+    }
+
+    public class TinNode
+    {
+        public int NodeID { get; set; }
+        public List<TinEdge> ConnectedEdges { get; set; }
+
+        public TinNode(int id)
+        {
+            NodeID = id;
+            ConnectedEdges = new List<TinEdge>();
+        }
+    }
+
+    public class TinEdge
+    {
+        public int EdgeID { get; set; }
+        public TinNode StartNode { get; set; }
+        public TinNode EndNode { get; set; }
+
+        public TinEdge(int id, TinNode startNode, TinNode endNode)
+        {
+            if (startNode.NodeID == endNode.NodeID)
+            {
+                throw new ArgumentException("Start node and end node cannot be the same.");
+            }
+
+            EdgeID = id;
+            StartNode = startNode;
+            EndNode = endNode;
+            startNode.ConnectedEdges.Add(this);
+            endNode.ConnectedEdges.Add(this);
+        }
+    }
+
+    public class TinTriangle
+    {
+        public int TriangleID { get; set; }
+        public TinEdge[] Edges { get; set; }
+
+        public TinTriangle(int id, TinEdge edge1, TinEdge edge2, TinEdge edge3)
+        {
+            // 检测边和节点是否完全相同
+            if (edge1 == edge2 || edge1 == edge3 || edge2 == edge3)
+            {
+                throw new ArgumentException("Edges cannot be completely the same in a triangle.");
+            }
+
+            Edges = new TinEdge[] { edge1, edge2, edge3 };
+            TriangleID = id;
+        }
+    }
+
+    public class TinDataset
+    {
+        public List<TinNode> Nodes { get; set; }
+        public List<TinEdge> Edges { get; set; }
+        public List<TinTriangle> Triangles { get; set; }
+
+        public TinDataset()
+        {
+            Nodes = new List<TinNode>();
+            Edges = new List<TinEdge>();
+            Triangles = new List<TinTriangle>();
+        }
+
+        public static int[] GetNodeIDsFromEdgeID(int edgeID, List<TinEdge> edges)
+        {
+            HashSet<int> nodeIDs = new HashSet<int>();
+
+            TinEdge targetEdge = edges.FirstOrDefault(edge => edge.EdgeID == edgeID);
+            if (targetEdge != null)
+            {
+                nodeIDs.Add(targetEdge.StartNode.NodeID);
+                nodeIDs.Add(targetEdge.EndNode.NodeID);
+            }
+
+            return nodeIDs.ToArray();
+        }
+
+        public static Tuple<int[], int[]> GetNodeAndEdgeIDsFromTriangleID(int triangleID, List<TinTriangle> triangles)
+        {
+            HashSet<int> uniqueNodeIDs = new HashSet<int>();
+            List<int> edgeIDs = new List<int>();
+
+            TinTriangle targetTriangle = triangles.FirstOrDefault(triangle => triangle.TriangleID == triangleID);
+            if (targetTriangle != null)
+            {
+                foreach (var edge in targetTriangle.Edges)
+                {
+                    uniqueNodeIDs.Add(edge.StartNode.NodeID);
+                    uniqueNodeIDs.Add(edge.EndNode.NodeID);
+                    edgeIDs.Add(edge.EdgeID);
+                }
+            }
+
+            return new Tuple<int[], int[]>(uniqueNodeIDs.ToArray(), edgeIDs.ToArray());
+        }
+
+        public static TinTriangle GetTriangleFromEdgeIDs(int[] edgeIDs, List<TinTriangle> triangles)
+        {
+            TinTriangle targetTriangle = triangles.FirstOrDefault(triangle =>
+            {
+                int[] triangleEdgeIDs = triangle.Edges.Select(edge => edge.EdgeID).ToArray();
+
+                return edgeIDs.Length == triangleEdgeIDs.Length &&
+                       edgeIDs.All(edgeID => triangleEdgeIDs.Contains(edgeID));
+            });
+
+            return targetTriangle;
+        }
+
+    }
+
+
+}
