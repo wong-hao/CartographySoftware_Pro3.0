467e09a4fb12e5a06cbffee2253fb13723ae8e55
haowong <haowong.seu@gmail.com>
2023-11-30 16:46:01 +0800

    Update HydlMaskProcessButton.cs

--- HydlMaskProcessButton.cs
+++ HydlMaskProcessButton.cs
@@ -1,24 +1,14 @@
 ﻿using ArcGIS.Core.CIM;
 using ArcGIS.Core.Data;
-using ArcGIS.Core.Geometry;
-using ArcGIS.Core.Internal.Geometry;
 using ArcGIS.Desktop.Framework.Contracts;
 using ArcGIS.Desktop.Framework.Dialogs;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
-using ArcGIS.Desktop.Internal.Mapping.TOC;
 using ArcGIS.Desktop.Mapping;
-using SMGI_Common;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
 using System.Linq;
-using System.Reflection.Emit;
-using System.Windows.Forms.VisualStyles;
-using ArcGIS.Core.Data.DDL;
-using ArcGIS.Core.Data.Exceptions;
-using ArcGIS.Desktop.Core;
-using ArcGIS.Desktop.Editing;
+using SMGI_Common;
+using System.Collections.Generic;
 
 namespace SMGI_Plugin_EmergencyMap
 {
@@ -26,25 +16,11 @@ namespace SMGI_Plugin_EmergencyMap
     {
         protected override async void OnClick()
         {
-            HYDLMaskingSetForm frmMask = new HYDLMaskingSetForm();
-            frmMask.ShowDialog();
-
-            bool usingMask = frmMask.UsingMask;
-            string maskingLyr = frmMask.MaskingLyr;
-            string maskedLyr = frmMask.MaskedLyr;
-            CommonMethods.UsingMask = usingMask;
-            CommonMethods.MaskLayer = frmMask.MaskedLyr;
-            CommonMethods.MaskedLayer = frmMask.MaskingLyr;
-            FeatureClass HYDAfcl = null;
-            FeatureClass HYDLfcl = null;
-            GroupLayer groupLyr = null;
 
             #region
             //目前还未找到方法判断不是临时数据
             await QueuedTask.Run(() =>
             {
-                Z3_Solver.Solve();
-
                 DataTable ruleDt = new DataTable();
                 try
                 {
@@ -72,15 +48,15 @@ namespace SMGI_Plugin_EmergencyMap
                 }
 
                 /*
-                foreach (DataColumn column in ruleDt.Columns)
-                {
-                    MessageBox.Show(column.ColumnName + "\t");
-                    foreach (DataRow row in ruleDt.Rows)
-                    {
-                        MessageBox.Show(row[column] + "\t");
-                    }
-                    Console.WriteLine(); // 换行
-                }*/
+ foreach (DataColumn column in ruleDt.Columns)
+ {
+     MessageBox.Show(column.ColumnName + "\t");
+     foreach (DataRow row in ruleDt.Rows)
+     {
+         MessageBox.Show(row[column] + "\t");
+     }
+     Console.WriteLine(); // 换行
+ }*/
 
                 // 创建TinDataset对象
                 TinDataset tinData = new TinDataset();
@@ -104,7 +80,6 @@ namespace SMGI_Plugin_EmergencyMap
                 TinTriangle triangle1 = new TinTriangle(1, edge1, edge2, edge3);
                 TinTriangle triangle2 = new TinTriangle(2, edge1, edge4, edge5);
                 TinTriangle triangle3 = new TinTriangle(3, edge3, edge4, edge6);
-
 
                 // 添加节点、边和三角形到数据结构中
                 tinData.Nodes.Add(node1);
@@ -194,176 +169,93 @@ namespace SMGI_Plugin_EmergencyMap
                 TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinNodesNotAll", "Node", false);
                 TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinEdgesNotAll", "Edge", false);
 
-                /*
+                                /*
                 List<TinNode> nodes = new List<TinNode>();
-               // 添加节点到 nodes 列表中
-               
-               List<TinEdge> edges = new List<TinEdge>();
-               // 添加边到 edges 列表中
-               
-               List<TinTriangle> triangles = new List<TinTriangle>();
-               // 添加三角形到 triangles 列表中
-               
-               TinDataset tinData = new TinDataset(nodes, edges, triangles);
-               
-               
-               int triangleNum = TinTriangle.GetTriangleNum("CCC_TinTriangle");
-               TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinNodesAll", "Node", true);
-               TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinEdgesAll", "Edge", true);
-               TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinNodesNotAll", "Node", false);
-               TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinEdgesNotAll", "Edge", false);
-               
-               
-               // 创建节点
-               
-               for (int i = 1; i <= 3 * triangleNum; i++)
-               {
-               TinNode node = new TinNode(i);
-               nodes.Add(node);
-               }
-               
-               // 创建边
-               
-               for (int i = 1; i <= 3 * triangleNum; i++)
-               {
-               if (i % 3 == 1)
-               {
-               TinEdge edge = new TinEdge(i, i, i + 1, tinData);
-               edges.Add(edge);
-               
-               }
-               else if (i % 3 == 2)
-               {
-               TinEdge edge = new TinEdge(i, i, i + 1, tinData);
-               edges.Add(edge);
-               }
-               else if (i % 3 == 0)
-               {
-               TinEdge edge = new TinEdge(i, i - 2, i, tinData);
-               edges.Add(edge);
-               }
-               else
-               {
-               break;
-               }
-               }
-               
-               // 创建三角形
-               
-               for (int i = 1; i <= triangleNum; i++)
-               {
-               TinTriangle triangle = new TinTriangle(i, 3 * i - 2, 3 * i - 1, 3 * i);
-               triangles.Add(triangle);
-               }
-               
-               // 添加节点、边和三角形到数据结构中
-               tinData.Nodes = nodes;
-               tinData.Edges = edges;
-               tinData.Triangles = triangles;
-               
-               // 输出节点、边和三角形的关联信息
-               foreach (var node in tinData.Nodes)
-               {
-               string connectedEdgesInfo = string.Join(", ", node.ConnectedEdgeIDs.Select(edgeID => edgeID.ToString()));
-               GApplication.writeLog(string.Format("Node ID: {0}, Connected Edge IDs: {1}", node.NodeID, connectedEdgesInfo), GApplication.DEBUG, false);
-               }
-               
-               foreach (var edge in tinData.Edges)
-               {
-               GApplication.writeLog(string.Format("Edge ID: {0}, Start Node ID: {1}, End Node ID: {2}", edge.EdgeID, edge.StartNodeID, edge.EndNodeID), GApplication.INFO, false);
-               }
-               
-               foreach (var tri in tinData.Triangles)
-               {
-               string edgesInfo = string.Join(", ", tri.EdgeIDs.Select(e => string.Format("Edge ID: {0}", e)));
-               GApplication.writeLog(string.Format("Triangle ID: {0}, Edges: {1}", tri.TriangleID, edgesInfo), GApplication.FATAL, false);
-               }
+                // 添加节点到 nodes 列表中
+
+                List<TinEdge> edges = new List<TinEdge>();
+                // 添加边到 edges 列表中
+
+                List<TinTriangle> triangles = new List<TinTriangle>();
+                // 添加三角形到 triangles 列表中
+
+                TinDataset tinData = new TinDataset(nodes, edges, triangles);
+
+
+                int triangleNum = TinTriangle.GetTriangleNum("CCC_TinTriangle");
+                TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinNodesAll", "Node", true);
+                TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinEdgesAll", "Edge", true);
+                TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinNodesNotAll", "Node", false);
+                TinTriangle.TinTriangleTransition("CCC_TinTriangle", "CCC_TinEdgesNotAll", "Edge", false);
+
+
+                // 创建节点
+
+                for (int i = 1; i <= 3 * triangleNum; i++)
+                {
+                TinNode node = new TinNode(i);
+                nodes.Add(node);
+                }
+
+                // 创建边
+
+                for (int i = 1; i <= 3 * triangleNum; i++)
+                {
+                if (i % 3 == 1)
+                {
+                TinEdge edge = new TinEdge(i, i, i + 1, tinData);
+                edges.Add(edge);
+
+                }
+                else if (i % 3 == 2)
+                {
+                TinEdge edge = new TinEdge(i, i, i + 1, tinData);
+                edges.Add(edge);
+                }
+                else if (i % 3 == 0)
+                {
+                TinEdge edge = new TinEdge(i, i - 2, i, tinData);
+                edges.Add(edge);
+                }
+                else
+                {
+                break;
+                }
+                }
+
+                // 创建三角形
+
+                for (int i = 1; i <= triangleNum; i++)
+                {
+                TinTriangle triangle = new TinTriangle(i, 3 * i - 2, 3 * i - 1, 3 * i);
+                triangles.Add(triangle);
+                }
+
+                // 添加节点、边和三角形到数据结构中
+                tinData.Nodes = nodes;
+                tinData.Edges = edges;
+                tinData.Triangles = triangles;
+
+                // 输出节点、边和三角形的关联信息
+                foreach (var node in tinData.Nodes)
+                {
+                string connectedEdgesInfo = string.Join(", ", node.ConnectedEdgeIDs.Select(edgeID => edgeID.ToString()));
+                GApplication.writeLog(string.Format("Node ID: {0}, Connected Edge IDs: {1}", node.NodeID, connectedEdgesInfo), GApplication.DEBUG, false);
+                }
+
+                foreach (var edge in tinData.Edges)
+                {
+                GApplication.writeLog(string.Format("Edge ID: {0}, Start Node ID: {1}, End Node ID: {2}", edge.EdgeID, edge.StartNodeID, edge.EndNodeID), GApplication.INFO, false);
+                }
+
+                foreach (var tri in tinData.Triangles)
+                {
+                string edgesInfo = string.Join(", ", tri.EdgeIDs.Select(e => string.Format("Edge ID: {0}", e)));
+                GApplication.writeLog(string.Format("Triangle ID: {0}, Edges: {1}", tri.TriangleID, edgesInfo), GApplication.FATAL, false);
+                }
 
                 */
 
-
-                var HYDLlyr = MapView.Active.Map.GetLayersAsFlattenedList().OfType<FeatureLayer>().Where(l => (l as FeatureLayer).Name == maskingLyr).FirstOrDefault() as FeatureLayer;
-
-                var HYDAlyr = MapView.Active.Map.GetLayersAsFlattenedList().OfType<FeatureLayer>().Where(l => (l as FeatureLayer).Name == maskedLyr).FirstOrDefault() as FeatureLayer;
-
-                var groups = MapView.Active.Map.GetLayersAsFlattenedList().OfType<GroupLayer>().Where(l => l is GroupLayer);
-
-                if (HYDLlyr != null)
-                {
-                    HYDLfcl = HYDLlyr.GetFeatureClass();
-                }
-                else
-                {
-                    MessageBox.Show("未找到水系线图层!", "提示");
-                    return;
-                }
-                if (HYDAlyr != null)
-                {
-                    HYDAfcl = HYDAlyr.GetFeatureClass();
-                }
-                else
-                {
-                    MessageBox.Show("未找到水系面图层!", "提示");
-                    return;
-                }
-                #endregion
-                #region
-                GroupLayer groupLyr1 = null;
-                GroupLayer groupLyr2 = null;
-                foreach (var group in groups)
-                {
-                    CompositeLayer g = group as CompositeLayer;
-                    for (int i = 0; i < g.Layers.Count; i++)
-                    {
-                        var l = g.Layers[i];
-                        if (l is FeatureLayer)
-                        {
-                            if ((l as FeatureLayer).Name == maskingLyr)
-                            {
-                                groupLyr1 = g as GroupLayer;
-                            }
-                            if ((l as FeatureLayer).Name == maskedLyr)
-                            {
-                                groupLyr2 = g as GroupLayer;
-                            }
-                        }
-                    }
-                }
-                if (groupLyr1.Equals(groupLyr2))
-                {
-                    groupLyr = groupLyr1;
-                }
-                else
-                {
-                    MessageBox.Show("不在同一个图层组!", "提示");
-                    return;
-                }
-                #endregion
-
-                //增加定义查询：不显示要素
-                CIMFeatureTable fd = (HYDAlyr.GetDefinition() as CIMFeatureLayer).FeatureTable;
-                string finitionExpression = fd.DefinitionExpression;
-                if (!finitionExpression.ToLower().Contains(string.Format("ruleid <> {0}", 1)))
-                {
-                    if (finitionExpression != "")
-                    {
-                        fd.DefinitionExpression = string.Format("({0}) and (ruleid <> {1})", finitionExpression, 1);
-                    }
-                    else
-                    {
-                        fd.DefinitionExpression = string.Format("ruleid <> {0}", 1);
-                    }
-                }
-
-                //图层掩膜方法 ArcGIS.Core.CIM.CIMBaselayer.LayerMasks
-                #region Mask feature
-                //Get the layer's definition
-                var lyrDefn = HYDLlyr.GetDefinition();
-                //Create an array of Masking layers (polygon only)
-                //Set the LayerMasks property of the Masked layer
-                lyrDefn.LayerMasks = new string[] { HYDAlyr.URI };
-                //Re-set the Masked layer's defintion
-                HYDLlyr.SetDefinition(lyrDefn);
                 #endregion
             });
             MessageBox.Show("水系结构线消隐完成！");
